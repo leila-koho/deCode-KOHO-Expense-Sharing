67a910a97ca00c2124538e48e3357c7f18559faf
Leila <leila@koho.ca>
2021-09-24 10:07:38 -0600

    and main components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,35 @@ import ExpensesDashboard from './Pages/ExpensesDashboard';
 import CreateExpensePage from './Pages/CreateExpensePage';
 import ExpenseBreakDownPage from './Pages/ExpenseBreakdownPage';
 import SingleExpensePage from './Pages/SingleExpensePage';
+import styled from 'styled-components';
+import WidthDrawSVG from './assets/logo-transparent.svg';
+import translation from './assets/translation.png'
+import Button from './components/Button.js';
+import Card from './components/Card.js';
+import Switch from '@mui/material/Switch';
+import TextField from '@mui/material/TextField';
+import Menu from '@mui/icons-material/Menu';
+
+const Wrapper = styled.div`
+  font-family: monospace;
+  color: blue;
+  margin: 2.5rem;
+`
+
+const Header = styled.div`
+  display: flex;
+  margin-bottom: 2rem;
+  justify-content: space-between;
+  color: #000000;
+`
+const  Dividor = styled.div`
+  widht: 1000000000000px;
+  border-top: 1px solid #c4c4c4
+`
+
+const TranslationIcon = styled.img`
+  padding-right: 0.5rem;
+`
 
 function App() {
   return (
@@ -29,6 +58,39 @@ function App() {
           <SingleExpensePage />
         </Route>
       </RouteSwitch>
+      <header className="App-header">
+        <Wrapper>
+          <Header>
+            <div>
+              <img
+                src={WidthDrawSVG}
+                alt=""
+              />
+            </div>
+            <div>
+              
+            <TranslationIcon src={translation} alt="" />
+            <Menu style={{ color: "#000000" }} />
+            </div>
+          </Header>
+          <Dividor />
+
+          <Card /> 
+
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <TextField id="filled-basic" label="Filled" variant="filled" />
+          <TextField id="standard-basic" label="Standard" variant="standard" />
+
+          <Button style="primary" onClick={() => alert('primary button clicked')}/> 
+          <Button style="secondary" onClick={() => alert('secondary button clicked')}/> 
+          <Button style="disabled" /> 
+
+          <Switch defaultChecked />
+          <Switch />
+          <Switch disabled defaultChecked />
+          <Switch disabled />
+      </Wrapper>
+      </header>
     </Router>
   );
 }
